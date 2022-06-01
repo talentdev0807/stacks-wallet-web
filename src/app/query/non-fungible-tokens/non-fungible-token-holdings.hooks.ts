40c673ae631e9869ec44bb4aca65b0271f000707
fbwoolf <fwoolf@blockstack.com>
2022-06-01 07:50:05 -0500

    refactor: suggested steps logic

---
+++ src/app/query/non-fungible-tokens/non-fungible-token-holdings.hooks.ts
@@ -0,0 +1,23 @@
+import BigNumber from 'bignumber.js';
+
+import { SoftwareWalletAccountWithAddress } from '@app/store/accounts/account.models';
+
+import {
+  useGetNonFungibleTokenHoldingsListQuery,
+  useGetNonFungibleTokenHoldingsQuery,
+} from './non-fungible-token-holdings.query';
+
+export function useNonFungibleTokenHoldings(address: string | undefined) {
+  const { data: nftHoldings } = useGetNonFungibleTokenHoldingsQuery(address);
+  return nftHoldings;
+}
+
+export function useAccountsNonFungibleTokenHoldings(
+  accounts: SoftwareWalletAccountWithAddress[] | undefined
+) {
+  const accountsNftHoldings = useGetNonFungibleTokenHoldingsListQuery(accounts);
+
+  return accountsNftHoldings.reduce((acc, nftHoldings) => {
+    return acc.plus(nftHoldings.data?.total || 0);
+  }, new BigNumber(0));
+}
